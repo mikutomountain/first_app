09a165dd56cb9ab9a15cae3d036f00579d65342b
mikuto <m3910.0706@gamail.com>
2026-02-07 02:32:29 +0900

    2/7

---
+++ app/components/ToDoList.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+const ToDoList = () => {
+  return (
+    <div>
+      <h2>Your ToDo List</h2>
+      <ul>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ToDoList;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
+import AddTask from "./components/AddTask";
+import ToDoList from "./components/ToDoList";
 
 export default function Home() {
-  return <div></div>;
+  return <main>
+    <h1>Nextjs 13 Todo App</h1>
+    <div>
+      <div>
+        <AddTask />
+        <ToDoList />
+      </div>
+    </div>
+  </main>;
 }
 
