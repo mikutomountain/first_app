e77b537029e42bfd7b4550efaf966b70d69cdbaa
mikuto <m3910.0706@gamail.com>
2026-02-08 03:43:46 +0900

    2/8

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const AddTask = () => {
   return (
-    <div>AddTask</div>
+    <form className="mb-4 space-y-3">
+        <input type="text" className="w-full border px-4 py-2 rounded-lg"/>
+        <button>Add Task </button>
+    </form>
   )
 }
 
